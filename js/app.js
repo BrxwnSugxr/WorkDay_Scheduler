2f38dc2efcf13e4bcd03de205cbd27da1eb64923
brxwnsugxr <jsdev.mas@gmail.com>
2023-12-19 12:32:54 +1100

    creating the hours

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 // step 1: container element id "timeBlockContainer"
 var timeBlockContainer = document.getElementById('timeBlocksContainer');
-// 
+// step 2: creating the hours
+var workingHours = Array.from({length:9}, (_,index)=> index +9);
+
